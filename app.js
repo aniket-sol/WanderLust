6da93fd506cb2c9d4d513f625b23813fd8e59652
Aniket Solanki <146086002+aniket-sol@users.noreply.github.com>
2023-12-14 11:13:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
 // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const dbUrl = process.env.MONGO_URL;;
+const dbUrl = process.env.MONGO_URL;
 
 main()
     .then(() => {
